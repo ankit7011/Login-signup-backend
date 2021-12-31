6343c5194a4e837c7aabdf8d4ff381965495e7e2
ankit7011 <51699299+ankit7011@users.noreply.github.com>
2022-01-01 04:28:20 +0530

    latest

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import { info } from "node-sass"
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
 
-mongoose.connect("mongodb+srv://ankit:Onetheway123@cluster0.t5vm4.mongodb.net/myaLoginRegisterDB?retryWrites=true&w=majority", {
+mongoose.connect('mongodb://localhost:27017/myaLoginRegisterDB', {
     useNewUrlParser: true,
     useUnifiedTopology:true
 },()=> {
@@ -21,11 +22,121 @@ const userSchema = mongoose.Schema({
     password:String
 })
 
+const parcelSchema = mongoose.Schema({
+  user : String,
+  info : String,
+  cost : String,
+  startloc : String,
+  destination : String,
+  SfullName : String,
+  SphoneNumber : String,
+  Saddress : String,
+  RfullName : String,
+  RphoneNumber : String,
+  Raddress : String,
+});
+
 const User=new mongoose.model("User",userSchema)
 
+const Parcel=new mongoose.model("Parcel",parcelSchema)
 
 
 //routes
+
+app.post("/add", (req, res) => {
+    console.log(req.body);
+    console.log("Call Recived Added");
+    const {
+      id,
+      user,
+      info,
+      cost,
+      startloc,
+      destination,
+      SfullName,
+      SphoneNumber,
+      Saddress,
+      RfullName,
+      RphoneNumber,
+      Raddress,
+    } = req.body;
+    
+    const entry = new Parcel({
+      user,
+      info,
+      cost,
+      startloc,
+      destination,
+      SfullName,
+      SphoneNumber,
+      Saddress,
+      RfullName,
+      RphoneNumber,
+      Raddress,
+    });
+    console.log(entry)
+    entry.save(err => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({ message: "Successfully Added" });
+        }
+    })
+})
+
+app.post("/con", (req, res) => {
+    const { name,email,password } = req.body
+    Parcel.find({ user: email }, (error, data) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("Finded Succesfully")
+            // console.log(data, email)
+            res.send(data)
+
+        }
+    })
+})
+
+app.post("/del", (req, res) => {
+    console.log("del Call Recived")
+    console.log(req.body)
+    const {
+      id,
+      user,
+      info,
+      cost,
+      startloc,
+      destination,
+      SfullName,
+      SphoneNumber,
+      Saddress,
+      RfullName,
+      RphoneNumber,
+      Raddress,
+    } = req.body;
+    Parcel.deleteOne({
+      user : user,
+      info: info,
+    cost :cost,
+      startloc : startloc,
+      destination : destination,
+      SfullName : SfullName,
+      SphoneNumber : SphoneNumber,
+      Saddress : Saddress,
+      RfullName : RfullName,
+      RphoneNumber : RphoneNumber,
+      Raddress : Raddress,
+    }, (err) => {
+        if (err) {
+            console.log(err)
+        } else
+        {
+            res.send({message : "DELETED"})
+            console.log("Deleted")
+        }
+    });
+})
 
 app.post("/login", (req, res) => {
     const { email, password } = req.body
